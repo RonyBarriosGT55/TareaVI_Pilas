b2b96f1ece2f7f52fccc66043f45b0b3c5912ae1
RonyBarriosGT55 <rbarriosb2@miumg.edu.gt>
2026-03-24 20:31:28 -0600

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -32,4 +32,69 @@ void mostrar(Nodo* tope, string nombrePila) {
   posicion++;
   }
 }
-//
+//Funcion mostrar estado actual
+void mostrarEstado(Nodo* acciones, Nodo* rehacer) {
+    mostrar(acciones, "Pila de acciones actuales");
+    mostrar(rehacer, "Pila de Reacher");
+}
+//Funcion Principal
+int main() {
+    Nodo* pilaAcciones = NULLL; // Pila principal
+    Nodo* pilaRehacer = NULL; // Pilas de acciones desechas.
+
+    int opcion;
+    string accion;
+    string accionMovida;
+
+cout <<"=============================================";
+cout <<"  Simulador de deshacer /Rehacer ";
+cout <<" Rony Alexander Barrios Carnet: 9941 99 127";
+
+do {
+    cout << "\n=========== MENU ===========\n";
+        cout << "1. Realizar accion\n";
+        cout << "2. Deshacer ultima accion (UNDO)\n";
+        cout << "3. Rehacer accion (REDO)\n";
+        cout << "4. Mostrar acciones actuales\n";
+        cout << "5. Mostrar pila de rehacer\n";
+        cout << "6. Mostrar ambas pilas\n";
+        cout << "7. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+        cin.ignore();
+
+  switch (opcion) {
+case 1:
+    cout <<"Ingresa la accion a realizar:";
+    getline(cin, accion);
+
+    push(pilaAcciones, accion);
+  // cada vez que se realiza la accion nueva se limpa la pila de rehacer
+  limpiar pila(pilaRehacer);
+  cout <<"Accion realizada correctamente. \n";
+  cout <<"Accion rreahacer fue eliminada." \n";
+  break;
+
+case 2:
+  if (pop(pilaAcciones, accionMovida)) {
+      push(pilaRehacer, accionMovida);
+      cout <<" se deshizo la ccion: " << accionMovida << endl;
+  } else {
+      cout << " No hay acciones para deshacer. \n";
+  }
+  break;
+
+case 3:
+  if(pop(pilaAcciones, accionMovida)) {
+      push(pilaRehacer, accionMovida);
+      cout << "se Deshizo la accion:"  << accionMovida <<endl;
+  }  else {
+        cout << " No hay acciones para deshacer. \n";
+  }
+    break;
+  
+
+cout
+
+
+
