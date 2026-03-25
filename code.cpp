3603c3caa68709e3b55faca8e440bf06bcd3abd8
RonyBarriosGT55 <rbarriosb2@miumg.edu.gt>
2026-03-24 20:17:34 -0600

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -4,3 +4,32 @@ using namespace std;
 //Estructura del Nodo
 struct Nodo{
   string accion;
+  Nodo* siguiente;
+};
+//funcion push
+void push(Nodo*& tope, string texto) {
+  Nodo* nuevo = new Nodo;
+nuevo->accion =texto;
+nuevo->siguiente = tope;
+tope = nuevo;
+}
+//Funcion mostrar
+//muestra el contenido de la pila
+void mostrar(Nodo* tope, string nombrePila) {
+  cout << " =================================================";
+  cout << nombrePila << endl;
+  cout << " =================================================;
+
+    if (tope == NULL) {
+  cout << "La pila esta vacia. ";
+  return;
+  }
+  Nodo* auxiliar = tope;
+  int posicion = 1;
+  while (auxiliar != NULL) {
+  cout << posicion << ". " << auxiliar->accion << endl;
+  auxiliar = auxiliar->siguiente;
+  posicion++;
+  }
+}
+//
